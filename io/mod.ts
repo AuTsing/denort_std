2bbdeeb0e7321332608b1d2a16e6a5f5e6850e93
AuTsing <763025696@qq.com>
2023-05-29 02:17:46 +0800

    bump to 0.4.2.3

--- io/mod.ts
+++ io/mod.ts
@@ -1,1 +1,7 @@
+/**
+ * 提供对安卓设备的输入输出操作。
+ *
+ * @module
+ */
+
 export const toast = Android.io.toast;
--- mod.ts
+++ mod.ts
@@ -1,4 +1,11 @@
 /// <reference types="./types.d.ts" />
+
+/**
+ * 提供通用类。
+ *
+ * @module
+ */
+
 export const Point = Android.Point;
 export const Rect = Android.Rect;
 export const UiObject = Android.UiObject;
